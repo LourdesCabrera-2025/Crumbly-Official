7f68265aae52094ba899a6e1944813001f98726c
LourdesCabrera-2025 <00034023@uca.edu.sv>
2025-10-25 19:16:56 -0600

    :memo: reescribiendo HomePage

--- client/src/views/public/HomePage.jsx
+++ client/src/views/public/HomePage.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import "../../styles/public/HomePage.css"
 import Navbar from "../../components/public/Navbar.jsx";
 import Orderbox from "../../assets/public/HomePage/Orderbox.jpg"
+import Macarons from "../../assets/public/HomePage/macaronsPackage.jpg"
 function HomePage() {
   return (
     <>
       <Navbar />
       <div className="container-body">
         <img src={Orderbox} alt="Productos Personalizados" className="image-banner" />
+      </div>
+      <hr className="border-line"/>
+      <div className="info-section">
+        <div className="image-info">
+          <img src={Macarons} alt="Macarons" className="h-100 w-fit" />
+        </div>
+        <div className="text-info">
+            <h4>Sweeten your Moments with Crumbly</h4>
+        </div>
       </div>
     </>
   );
