6f28d98b6a7c5b951bdb0611d194e4dd27cd6050
DanielAlexander912 <143742338+DanielAlexander912@users.noreply.github.com>
2025-11-14 15:36:53 -0600

    feat: Adding Valoracion model

--- server/app/Models/Valoracion.php
+++ server/app/Models/Valoracion.php
@@ -2,9 +2,32 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Valoracion extends Model
 {
-    //
+    use HasFactory;
+    
+    protected $table = 'Valoracion';
+    protected $primaryKey = 'id_valoración';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'id_cliente', 'id_producto', 'puntuacion', 'comentario', 'fecha'
+    ];
+
+    protected $casts = [
+        'fecha' => 'datetime'
+    ];
+
+    public function cliente()
+    {
+        return $this->belongsTo(Cliente::class, 'id_cliente', 'id_cliente')
+    }
+
+    public function producto()
+    {
+        return $this->belongsTo(Producto::class, 'id_producto', 'id_producto')
+    }
 }
