6a1d6b20865b67aaf0f858ed71bdb2c65cfa7473
DanielAlexander912 <143742338+DanielAlexander912@users.noreply.github.com>
2025-11-14 15:41:34 -0600

    feat: Adding Administrador and Valoracion routes

--- server/routes/api.php
+++ server/routes/api.php
@@ -4,9 +4,25 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\ProductoController;
+use App\Http\Controllers\AdministradorController;
+use App\Http\Controllers\ValoracionController;
 
 Route::get('/productos', [ProductoController::class, 'index']);
 Route::get('/productos/{id}', [ProductoController::class, 'show']);
 Route::post('/productos', [ProductoController::class, 'store']);
 Route::put('/productos/{id}', [ProductoController::class, 'update']);
 Route::delete('/productos/{id}', [ProductoController::class, 'destroy']);
+
+//Rutas para Administrador
+Route::get('/administradores', [AdministradorController::class, 'index']);
+Route::get('/administradores/{id}', [AdministradorController::class, 'show']);
+Route::post('/administradores', [AdministradorController::class, 'store']);
+Route::put('/administradores/{id}', [AdministradorController::class, 'update']);
+Route::delete('/administradores/{id}', [AdministradorController::class, 'destroy']);
+
+//Rutas para Valoracion
+Route::get('/valoraciones', [ValoracionController::class, 'index']);
+Route::get('/valoraciones/{id}', [ValoracionController::class, 'show']);
+Route::post('/valoraciones', [ValoracionController::class, 'store']);
+Route::put('/valoraciones/{id}', [ValoracionController::class, 'update']);
+Route::delete('/valoraciones/{id}', [ValoracionController::class, 'destroy']);
