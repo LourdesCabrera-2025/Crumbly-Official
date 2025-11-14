a544b4279566b418f4e8cd70f97216b1a34a328e
DanielAlexander912 <143742338+DanielAlexander912@users.noreply.github.com>
2025-11-14 15:36:04 -0600

    feat: Adding Administrador model

--- server/app/Models/Administrador.php
+++ server/app/Models/Administrador.php
@@ -2,9 +2,34 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Administrador extends Model
 {
-    //
+    use HasFactory;
+    
+    protected $table = 'Administrador';
+    protected $primaryKey = 'id_admin';
+    public $incrementing = false;
+    public $timestamps = false;
+
+    protected $fillable = [
+        'nombre', 'id_tipo_usuario'
+    ];
+
+    public function tipoUsuario()
+    {
+        return $this->belongsTo(TipoUsuario::class, 'id_tipo_usuario', 'id_tipo_usuario')
+    }
+
+    public function productos()
+    {
+        return $this->hasMany(Producto::class, 'id_admin', 'id_admin')
+    }
+
+    public function logs()
+    {
+        return $this->hasMany(LogAcciones::class, 'id_admin', 'id_admin')
+    }
 }
